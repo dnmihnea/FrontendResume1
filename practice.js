4d67b0d356dc811ff3ab847b8671a8ffb3582275
dnmihnea <mihnea.dinu100@gmail.com>
2022-08-03 14:56:57 +0100

    added js file

---
+++ practice.js
@@ -0,0 +1,12 @@
+"use strict";
+
+console.log("Alex");
+console.info("Dinu");
+console.warn("Ploiesti");
+console.error("Leo");
+
+let myCar = "Audi";
+let myModel = "A5";
+console.log("My favourite car brand is ", myCar, " and the model is ", myModel);
+
+console.log("There is %cno crying%c in baseball.", "color: orange;font-family:fantasy; font-style: Bold; background-color: black;padding: 10px");
